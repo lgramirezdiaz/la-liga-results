2507e4ff0ab2548d20a0e67006008cdf6b4aaf8d
Janice Prendas <janice.prendas.roldan@gmail.com>
2019-05-21 14:27:49 -0600

    Mapa para cada equipo

--- la_liga_results/resources/views/equipos/show.blade.php
+++ la_liga_results/resources/views/equipos/show.blade.php
@@ -7,6 +7,53 @@
   }
 </style>
 <title>{{ $equipo->Nombre }}</title>
+
+<style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 300px;
+      }
+      /* Optional: Makes the sample page fill the window. 
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }*/
+</style>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCOJbu3uDOvYmkazk3onNIYxvFKn_C3eo4"></script>
+<script>
+      function initialize() {
+        var pais = { lat: {{ $equipo->Latitud }}, lng: {{ $equipo->Longitud }} };  
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 7,
+          center: pais
+        });
+
+        // This event listener calls addMarker() when the map is clicked.
+        google.maps.event.addListener(map, 'click', function(event) {
+          addMarker(event.latLng, map);
+        });
+       //Esto se debe hacer para cada equipo
+        var ayr = { lat: {{ $equipo->Latitud }}, lng: {{ $equipo->Longitud }} }; //Latitud y longitud del equipo       
+        addMarker(ayr, "{{ $equipo->Nombre }}", map);
+      }
+
+      // Adds a marker to the map.
+      function addMarker(location, label, map) {
+        // Add the marker at the clicked location, and add the next-available label
+        // from the array of alphabetical characters.
+        var marker = new google.maps.Marker({
+          position: location,
+          label: label,
+          map: map
+        });
+      }
+
+      google.maps.event.addDomListener(window, 'load', initialize);
+</script>
+
 <div class="card uper">
   <div class="card-header">
     Información del Equipo
@@ -22,17 +69,10 @@
       </div><br />
     @endif
         <div class="form-group">
-          <label for="name">Nombre</label>
-          <input type="text" class="form-control" name="nombre" value= "{{ $equipo->Nombre }}" readonly />
+          <label class="form-control" name="nombre" readonly>{{ $equipo->Nombre }}</label>
         </div>
-        <div class="form-group">
-          <label for="price">Latitud</label>
-          <input type="text" class="form-control" name="latitud" value= "{{ $equipo->Latitud }}" readonly/>
-        </div>
-        <div class="form-group">
-          <label for="quantity">Longitud:</label>
-          <input type="text" class="form-control" name="longitud" value= "{{ $equipo->Longitud }}" readonly/>
-        </div>            
+         
+        <div id="map" class="form-group">Mapa</div>           
   </div>
 </div>
 @endsection
